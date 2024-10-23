4594e1cb05fb99af4d88e1d15c5516fc30197dfb
Tanner Satchwell <tsatchwell@fileshadow.com>
2024-10-23 12:24:31 -0600

    get info, state

--- src/client.ts
+++ src/client.ts
@@ -7,22 +7,21 @@ import { WLEDState } from "../types/wled-state";
 class client {
   private axiosInstance: AxiosInstance;
   public readonly isReady: Promise<boolean> = Promise.resolve(false);
-  public readonly info: WLEDInfo | {} = {};
-  public readonly state: WLEDState | {} = {};
+  public info: WLEDInfo | {} = {};
+  public state: WLEDState | {} = {};
 
   constructor({ baseUrl, ssl = false, port = 80 }: ClientConstructor) {
     this.axiosInstance = axios.create({
-      baseURL: `${ssl ? "https" : "http"}://${baseUrl}:${port}/api`,
+      baseURL: `${ssl ? "https" : "http"}://${baseUrl}:${port}`,
     });
     axiosRetry(this.axiosInstance, { retries: 3 });
   }
 
-  public async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+  public async getInfo(config?: AxiosRequestConfig): Promise<WLEDInfo> {
     try {
-      const response: AxiosResponse<T> = await this.axiosInstance.get(
-        url,
-        config
-      );
+      const response: AxiosResponse<WLEDInfo> = await this.axiosInstance.get('/info', config);
+      console.debug("getInfo response:", response.data);
+      this.info = response.data;
       return response.data;
     } catch (error) {
       this.handleError(error);
@@ -30,17 +29,11 @@ class client {
     }
   }
 
-  public async post<T>(
-    url: string,
-    data: any,
-    config?: AxiosRequestConfig
-  ): Promise<T> {
+  public async getState(config?: AxiosRequestConfig): Promise<WLEDState> {
     try {
-      const response: AxiosResponse<T> = await this.axiosInstance.post(
-        url,
-        data,
-        config
-      );
+      const response: AxiosResponse<WLEDState> = await this.axiosInstance.get('/state', config);
+      console.debug("getState response:", response.data);
+      this.state = response.data;
       return response.data;
     } catch (error) {
       this.handleError(error);
