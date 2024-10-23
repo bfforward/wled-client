acf634ade41835e37499156cf4fef1ec80238eb4
Tanner Satchwell <tsatchwell@fileshadow.com>
2024-10-23 11:44:51 -0600

    potential fix

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 import client from './client';
-export * from '../types/client';
-export * from '../types/wled-constants';
-export * from '../types/wled-info';
-export * from '../types/wled-segment';
-export * from '../types/wled-state';
+export * from '../types/client.d';
+export * from '../types/wled-constants.d';
+export * from '../types/wled-info.d';
+export * from '../types/wled-segment.d';
+export * from '../types/wled-state.d';
 const wledClient = client;
 export default wledClient;
