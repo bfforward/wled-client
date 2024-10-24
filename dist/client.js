cb562a5e4ebeef063222f78a613d29989b92db23
Tanner Satchwell <tsatchwell@fileshadow.com>
2024-10-23 21:36:05 -0600

    build

--- dist/client.js
+++ dist/client.js
@@ -2,19 +2,36 @@ import axios from "axios";
 import axiosRetry from "axios-retry";
 class client {
     axiosInstance;
-    isReady = Promise.resolve(false);
     info = {};
     state = {};
+    effects = [];
+    palettes = [];
     constructor({ baseUrl, ssl = false, port = 80 }) {
         this.axiosInstance = axios.create({
-            baseURL: `${ssl ? "https" : "http"}://${baseUrl}:${port}`,
+            baseURL: `${ssl ? "https" : "http"}://${baseUrl}:${port}/json`,
         });
         axiosRetry(this.axiosInstance, { retries: 3 });
+    }
+    async init() {
+        await this.getAll();
+    }
+    async getAll(config) {
+        try {
+            const response = await this.axiosInstance.get('/', config);
+            this.info = response.data.info;
+            this.state = response.data.state;
+            this.effects = response.data.effects;
+            this.palettes = response.data.palettes;
+            return response.data;
+        }
+        catch (error) {
+            this.handleError(error);
+            throw error;
+        }
     }
     async getInfo(config) {
         try {
             const response = await this.axiosInstance.get('/info', config);
-            console.debug("getInfo response:", response.data);
             this.info = response.data;
             return response.data;
         }
@@ -26,8 +43,41 @@ class client {
     async getState(config) {
         try {
             const response = await this.axiosInstance.get('/state', config);
-            console.debug("getState response:", response.data);
             this.state = response.data;
+            return response.data;
+        }
+        catch (error) {
+            this.handleError(error);
+            throw error;
+        }
+    }
+    async setState(newState, config) {
+        try {
+            const response = await this.axiosInstance.post('/state', newState, config);
+            const updatedState = { ...this.state, ...newState };
+            this.state = updatedState;
+            return updatedState;
+        }
+        catch (error) {
+            this.handleError(error);
+            throw error;
+        }
+    }
+    async getEffects(config) {
+        try {
+            const response = await this.axiosInstance.get('/eff', config);
+            this.effects = response.data;
+            return response.data;
+        }
+        catch (error) {
+            this.handleError(error);
+            throw error;
+        }
+    }
+    async getPalettes(config) {
+        try {
+            const response = await this.axiosInstance.get('/pal', config);
+            this.palettes = response.data;
             return response.data;
         }
         catch (error) {
